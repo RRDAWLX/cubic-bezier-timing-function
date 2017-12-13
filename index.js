5062b9c0749bf85bf2c38fb4866d9963a036d233
longxiang.wang <longxiang.wang@vipshop.com>
2017-12-13 18:09:15 +0800

    'm'

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-let quadrate2 = document.querySelector('.quadrate2');
+let quadrate1 = document.querySelector('.quadrate1'),
+  quadrate2 = document.querySelector('.quadrate2'),
+  x1 = Math.random().toPrecision(3),
+  y1 = Math.random().toPrecision(3),
+  x2 = Math.random().toPrecision(3),
+  y2 = Math.random().toPrecision(3),
+  duration = 1;   // 动画时长，单位 s。
 
-let f = cubicBezierTimingFunction(0.1, 0.13, 0.43333, 0.57),
+let f = cubicBezierTimingFunction(x1, y1, x2, y2),
     startTime = Date.now();
-// coefficients(0.1, 0.43, 0.14, 0.65);
+coefficients(x1, y1, x2, y2);
 console.log(`f(0): ${f(0)}, f(1): ${f(1)}`);
 
+quadrate1.style.animation = `trans ${duration}s cubic-bezier(${x1}, ${y1}, ${x2}, ${y2}) forwards`;
+animate();
+
 function animate () {
-    let x = (Date.now() - startTime) / 1000;
+    let x = (Date.now() - startTime) / (duration * 1000);
     if (x > 1) {
         x = 1;
     }
@@ -19,10 +28,12 @@ function animate () {
     }
 }
 
+/**
+ * @desc
+ */
 function coefficients(x1, y1, x2, y2) {
-    console.log(`a: ${3 * x1 - 3 * x2 + 1}, b: ${3 * x2 - 6 * x1}, c: ${3 * x1}`);
-    console.log(`a: ${3 * (3 * x1 - 3 * x2 + 1)}, b: ${2 * (3 * x2 - 6 * x1)}, c: ${3 * x1}`);
-    console.log(`delta: ${x1 * x1 + x2 * x2 - x1 * x2 - x1}`);
+    console.log(`x1: ${x1}, y1: ${y1}, x2: ${x2}, y2: ${y2}`);
+    console.log(`x函数系数\n a: ${3 * x1 - 3 * x2 + 1}, b: ${3 * x2 - 6 * x1}, c: ${3 * x1}`);
+    console.log(`x函数导函数系数\n a: ${3 * (3 * x1 - 3 * x2 + 1)}, b: ${2 * (3 * x2 - 6 * x1)}, c: ${3 * x1}`);
+    // console.log(`delta: ${x1 * x1 + x2 * x2 - x1 * x2 - x1}`);
 }
-
-animate();
